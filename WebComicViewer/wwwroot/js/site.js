53f64d8933cce4dca64c67c7cb402c25ec875a77
Jacob Fliss <newgmobile@gmail.com>
2022-12-23 14:59:14 -0500

    side by side added to landscape mode
    height of pages matches view height

--- WebComicViewer/wwwroot/js/site.js
+++ WebComicViewer/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    CheckWH();
+});
+
+$(window).resize(function () {
+    CheckWH();
+});
+
+function CheckWH() {
+    var width = $(window).width();
+    var height = $(window).height() + 400;
+
+    if (width < height) {
+        console.log(`changing objects to 100% width. W:${width} H:${height}`);
+        $(".comic_page").width('100%');
+        $("#cover_spacer").hide();
+        $(".spacers").hide();
+    }
+    else {
+        console.log(`changing objects to 50% width. W:${width} H:${height}`);
+        $(".comic_page").width('50%');
+        $("#cover_spacer").show();
+        $(".spacers").show();
+    }
+}
